9124c2dcd9bc8bba72dc8620d17044a42285c208
Alice LIN <alilin@e2r3p8.clusters.42paris.fr>
2022-11-07 12:10:49 +0100

    premices

---
+++ includes/ft_ping.h
@@ -0,0 +1,13 @@
+#ifndef FT_PING_H
+# define FT_PING_H
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include ""
+
+
+
+#endif
---
+++ srcs/ft_getopt.c
@@ -0,0 +1,8 @@
+#include "../includes/ft_ping.h"
+
+char *ft_getopt(int ac, char **av, char *options) {
+    int i = 1;
+    while() {
+
+    }
+}
