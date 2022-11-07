c9828aee8faa5ca2745a4c423abd20b12dd489d1
Alice LIN <alilin@e2r3p8.clusters.42paris.fr>
2022-11-07 17:45:32 +0100

    started getopt

--- srcs/ft_getopt.c
+++ srcs/ft_getopt.c
@@ -6,24 +6,17 @@
 /*   By: alilin <alilin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 12:55:07 by alilin            #+#    #+#             */
-/*   Updated: 2022/11/07 14:15:58 by alilin           ###   ########.fr       */
+/*   Updated: 2022/11/07 17:04:31 by alilin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ping.h"
 
-// char *ft_getopt(int ac, char **av, char *options) {
-//     int i = 1;
-//     while(i) {
-//         printf("%s\n", "MEUF TA RIEN FAIT")
-//     }
-// }
-
-int main(int argc, char const *argv[])
-{
-    (void)argc;
-    (void)argv;
-    printf("%s\n", "MEUF TA RIEN FAIT");
-    ft_putstr_fd("connasse\n", 1);
-    return (0);
+char *ft_getopt(int ac, char **av, char **options) {
+    (void)ac;
+    (void)av;
+    for(int j = 0; options[j]; j++) {
+        printf("%s\n", options[j]);
+    }
+    return (NULL);
 }
--- srcs/ft_ping.c
+++ srcs/ft_ping.c
@@ -6,7 +6,19 @@
 /*   By: alilin <alilin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 12:54:59 by alilin            #+#    #+#             */
-/*   Updated: 2022/11/07 12:55:00 by alilin           ###   ########.fr       */
+/*   Updated: 2022/11/07 17:30:20 by alilin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_ping.h"
+
+int main(int ac, char  **av)
+{
+    if (ac < 2) {
+        fprintf(stderr, "ping: usage error: Destination address required\n");
+        exit(EXIT_FAILURE);
+    }
+    ft_getopt(ac, av, );
+    (void)av;
+    return (0);
+}
