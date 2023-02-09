db5b08bc5a7928efa451831e8d05c02926e076a0
Alice LIN <alilin@e1r10p2.clusters.42paris.fr>
2023-02-09 17:05:59 +0100

    playing with sleep and usleep

--- srcs/ft_ping.c
+++ srcs/ft_ping.c
@@ -6,7 +6,7 @@
 /*   By: alilin <alilin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 12:54:59 by alilin            #+#    #+#             */
-/*   Updated: 2023/02/09 17:03:48 by alilin           ###   ########.fr       */
+/*   Updated: 2023/02/09 17:05:37 by alilin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,7 +116,7 @@ void	ping_loop(t_ping_env *env)
 			disp_stats(env);
 			g_send[1] = false;
 		}
-		usleep(1000);
+		sleep(0.7);
 	}
 	get_statistic(env);
 }
